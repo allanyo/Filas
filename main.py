5f0d8a28dbb9d5163f01a13fbb86f0bd3d84bc2e
Allanyo <allanyosantos@gmail.com>
2023-07-17 00:38:01 -0300

    Add Comentarios
    Acrescente alguns comentarios

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from FilaDePrioridade import FilaDePrioridade
 
 x = FilaDePrioridade()
-x.inserir(10)
-x.inserir(16)
-x.inserir(1)
-x.inserir(6)
-x.inserir(14)
+x.inserir(10) #Inserção de valor 
+x.inserir(16) #Inserção de valor 
+x.inserir(1) #Inserção de valor 
+x.inserir(6) #Inserção de valor 
+x.inserir(14) #Inserção de valor 
 
-print(x.itens)
+print(x.itens) #Impressão da Fila  no  momento  atual 
 
-x.remover()
-print(x.itens)
+x.remover() #remoção de acordo com a prioriade 
+print(x.itens)#Impressão da Fila  no  momento  atual 
 
-x.remover()
+x.remover() #remoção de acordo com a prioriade 
 
-print(x.itens)
+print(x.itens)#Impressão da Fila no  momento  atual 
 
-x.remover()
-print(x.itens)
-x.remover()
-print(x.itens)
+x.remover() #remoção de acordo com a prioriade 
+print(x.itens) #Impressão da Fila  no  momento  atual 
+x.remover() #remoção de acordo com a prioriade 
+print(x.itens) #Impressão da Fila  no  momento  atual 
 
